774182e2a4b84caf8e7c18d4eb9633fe26066108
Rebecca Bengtsson <rebecca.bengtsson@hotmail.com>
2025-06-17 14:36:20 +0200

    Footer function

--- Components/Footer.php
+++ Components/Footer.php
@@ -4,7 +4,28 @@ function Footer()
 {
     ?>
     <footer class="myFoot">
-        <div>HEJ</div>
+
+        <div class="footer-list-icon">
+            <div class="foot-list">
+                <div class="foot-icon">
+                    <a href="#!" class="foot-brand">
+                        <i class="fa-brands fa-github"></i>
+                    </a>
+                </div>
+                <div class="foot-icon">
+                    <a href="#!" class="foot-brand">
+                        <i class="fa-brands fa-facebook"></i>
+                    </a>
+                </div>
+                <div class="foot-icon">
+                    <a href="#!" class="foot-brand">
+                        <i class="fa-brands fa-x-twitter"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
     </footer>
 
     <?php
